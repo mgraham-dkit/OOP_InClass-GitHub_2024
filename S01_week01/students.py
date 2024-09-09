ad94310a9d62fd0d84c5a17431b29de889620f08
michelle <michelle.graham@dkit.ie>
2024-09-09 11:03:34 +0100

    Basic Student class

---
+++ S01_week01/students.py
@@ -0,0 +1,12 @@
+class Student:
+    id = "D000"
+    name = "Michelle"
+    module = "OO Programming"
+
+
+if __name__ == "__main__":
+    person1 = Student()
+    print(person1.name)
+    person2 = Student()
+    print(person2)
+    person3 = Student()
---
+++ S01_week01/using_student.py
@@ -0,0 +1,5 @@
+from students import Student
+
+
+p1 = Student()
+print(p1)
