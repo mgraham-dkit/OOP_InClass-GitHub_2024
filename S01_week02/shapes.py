4537b040ba4583e3996990c8bbb621afa831e289
michelle <michelle.graham@dkit.ie>
2024-09-23 01:12:28 +0100

    Feature: Added encapsulation to colour field in Rectangle.

--- S01_week02/shapes.py
+++ S01_week02/shapes.py
@@ -1,12 +1,22 @@
 class Rectangle:
+    valid_colours = ["red", "blue", "yellow", "purple", "transparent"]
+
     def __init__(self, length, width, colour="Transparent"):
         self.length = length
         self.width = width
-        self.colour = colour
+        if colour in Rectangle.valid_colours:
+            self._colour = colour
 
     def display(self):
-        print(f"Rectangle[length={self.length}, width={self.width}, colour={self.colour}")
+        print(f"Rectangle[length={self.length}, width={self.width}, colour={self._colour}")
 
     def calc_area(self):
         area = self.length * self.width
         return area
+
+    def get_colour(self):
+        return self._colour
+
+    def set_colour(self, colour):
+        if colour in Rectangle.valid_colours:
+            self._colour = colour
