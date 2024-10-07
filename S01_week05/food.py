5dab91715034e76e69d11b49a57ceea6b4efcab5
michelle <michelle.graham@dkit.ie>
2024-10-07 21:36:47 +0100

    Documentation: Added some comments to Pizza class

--- S01_week05/food.py
+++ S01_week05/food.py
@@ -1,4 +1,6 @@
 class Pizza:
+    # Shared information - one copy shared by all Pizza instances
+    # Reference these using Pizza.variable_name
     size_options = {
         "small": 10,
         "medium": 12,
@@ -6,22 +8,25 @@ class Pizza:
         "extra large": 18
     }
 
-    topping_options = ["pineapple", "mozzarella", "brie", "goat cheese", "tomato", "pepperoni", "ham", "mushroom", "chicken", "peppers", "onions", "meatballs", "bbq sauce", "tomato sauce"]
+    topping_options = ["pineapple", "mozzarella", "brie", "goat cheese", "tomato", "pepperoni", "ham", "mushroom",
+                       "chicken", "peppers", "onions", "meatballs", "bbq sauce", "tomato sauce"]
     max_topping_count = 10
     topping_price = 0.85
 
+    # Constructor - deals with information for a specific Pizza instance
     def __init__(self, size="medium", toppings=None):
+        # As there are only a specific set of allowable sizes,
+        # confirm that the supplied one is allowable before using it
+        # If it's not valid, use a default value
         if not Pizza.validate_size(size):
             size = "medium"
         self._size = size
 
+        # If no toppings were supplied, create an empty list
+        # to hold any potential toppings added in future
         if toppings is None:
             toppings = []
         self._toppings = toppings
-
-    def set_size(self, size):
-        if Pizza.validate_size(size):
-            self._size = size
 
     @staticmethod
     def validate_size(size):
@@ -29,6 +34,10 @@ class Pizza:
             return True
         else:
             return False
+
+    def set_size(self, size):
+        if Pizza.validate_size(size):
+            self._size = size
 
     def get_size(self):
         return self._size
