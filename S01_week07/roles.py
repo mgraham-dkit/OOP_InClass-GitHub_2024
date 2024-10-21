905b7bbd2de3a53ae49691375b1f8a20766c29a5
michelle <michelle.graham@dkit.ie>
2024-10-21 10:38:52 +0100

    Feature: Added type hinting in Student class of roles module.

--- S01_week07/roles.py
+++ S01_week07/roles.py
@@ -30,16 +30,13 @@ class Person:
 
 
 class Student(Person):
-    # This should be a static list of valid course names and should be private.
-    # Private is achieved by using _ on the variable name
-    # This is a static variable because it's been made outside all methods and is not referenced with self.
-    _courses = ["computing systems and operations", "computing", "software development", "business", "irish history", "data science"]
-    # This is static so that it will be shared over all Students (NOT all Persons!)
-    _student_count = 0
+    _courses: list[str] = ["computing systems and operations", "computing", "software development", "business", "irish history", "data science"]
+    _student_count: int = 0
 
-    # Name and dob are required because there are no values provided for them here
-    # Course is set to None so it can be set to the list content in the __init__
-    def __init__(self, name, dob, course=None, year=1):
+    # Type hinting on method signature
+    # -> States what the method will return
+    # : States what type the variable contains
+    def __init__(self, name: str, dob: dt.datetime, course: str = None, year: int = 1) -> None:
         # Pass the Person-related information to the Person constructor so it can handle storing those values
         super().__init__(name, dob)
 
@@ -59,42 +56,27 @@ class Student(Person):
             year = 1
         self.year = year
 
-    # To clean up our validation, we can write a separate method to check if a supplied course name is allowable
-    # This is a static method as it's not operating on any of the object's information
     @staticmethod
-    def validate_course(course_name):
-        # Check if the name is none before we do anything - this avoids crashing on the lower() call
-        # Use lower() to make sure we don't need to consider case in the comparison
-        # If the name isn't in the allowable list, return false
+    def validate_course(course_name: str) -> bool:
         if course_name is None or course_name.lower() not in Student._courses:
             return False
-
-        # If it passes the above checks, return true because the name is valid
         return True
 
-    # As specified, this one is defined explicitly as just a static method, not a class method
     @staticmethod
-    # Create a static method to get the list of valid courses
-    def get_valid_courses():
-        # We don't give back the actual list of valid courses
-        # as we don't want to let others have a way to change the list directly
-        # Make a copy so that any changes they make would not impact the list here
+    def get_valid_courses() -> list[str]:
         valid_courses = Student._courses.copy()
         return valid_courses
 
     # As specified, this one is a class method, not just a static method
     @classmethod
-    def add_course(cls, course_name):
-        # Convert the name to lowercase to match the other courses in the list
+    def add_course(cls, course_name: str) -> bool:
         course_name = course_name.lower()
-        # If the supplied course is not in the list, add it and return True
         if course_name not in cls._courses:
             cls._courses.append(course_name)
             return True
 
-        # Otherwise return false
         return False
 
-    def display(self):
+    def display(self) -> None:
         super().display()
         print(f"ID: {self.student_id}, in year {self.year} of {self._course}")
