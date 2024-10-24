836c39be6c119900f28b561e97bdd63638eabd0e
michelle <michelle.graham@dkit.ie>
2024-10-24 20:54:13 +0100

    Feature: Added text-based logic to Vehicle and Bike classes.

--- S01_week07/using_vehicles.py
+++ S01_week07/using_vehicles.py
@@ -1,4 +1,5 @@
 from vehicles import Vehicle
+from vehicles import Bike
 
 
 vroom = Vehicle("Honda", "Red", 7)
@@ -8,3 +9,10 @@ if vroom == vroom_vroom:
     print("The same!!")
 else:
     print("What happened???")
+
+bike1 = Bike("Carrera", "Red", 5, 12)
+bike2 = Bike("Apollo", "Blue", 4, 10)
+
+vehicles = [vroom, vroom_vroom, bike1, bike2]
+for vehicle in vehicles:
+    print(vehicle)
--- S01_week07/vehicles.py
+++ S01_week07/vehicles.py
@@ -1,10 +1,13 @@
+from __future__ import annotations
+
+
 class Vehicle:
-    def __init__(self, make, colour, num_seats=5):
+    def __init__(self, make: str, colour: str, num_seats: int = 5) -> None:
         self.make = make
         self.colour = colour
         self.num_seats = num_seats
 
-    def __eq__(self, other):
+    def __eq__(self, other: Vehicle) -> bool:
         if not isinstance(other, Vehicle):
             return NotImplemented
 
@@ -16,3 +19,33 @@ class Vehicle:
             return False
 
         return True
+
+    def __repr__(self) -> str:
+        return f"Vehicle[make={self.make}, colour={self.colour}, num_seats={self.num_seats}]"
+
+    def __str__(self) -> str:
+        return f"{self.make} vehicle in {self.colour} with {self.num_seats} seats"
+
+
+class Bike(Vehicle):
+    def __init__(self, make, colour, num_seats, num_gears=3):
+        super().__init__(make, colour, num_seats)
+        self.num_gears = num_gears
+
+    def __eq__(self, other: Bike):
+        if not isinstance(other, Bike):
+            return NotImplemented
+
+        if not super().__eq__(other):
+            return False
+
+        if self.num_gears != other.num_gears:
+            return False
+
+        return True
+
+    def __repr__(self):
+        return f"Bike[make={self.make}, colour={self.colour}, num_seats={self.num_seats}, num_gears={self.num_gears}]"
+
+    def __str__(self):
+        return f"{self.make} bike in {self.colour} with {self.num_seats} seats and {self.num_gears} gears."
