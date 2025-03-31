7a55d72eab6a530cf5c8c551a4b869f5a504f87d
michelle <michelle.graham@dkit.ie>
2025-03-31 15:25:24 +0100

    Feature(sockets): Multi-exchange iterative server providing echo action
    Server:
     - uses a stream (TCP-based) socket
     - listens on port 7878
     - Deals with one client at a time (from start to end)
     - Repeatedly accepts clients
     - Is providing part of combo service from exercise sheet

--- S02_week10/sample_echo_server.py
+++ S02_week10/sample_echo_server.py
@@ -10,10 +10,26 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
     while True:
         conn, addr = server_socket.accept()
-
         with conn:
-            data = conn.recv(1024)
-            decoded = data.decode("utf-8")
-            print(f"Message received from {addr}: {decoded}")
+            client_session = True
 
-            conn.sendall(bytes(decoded, "utf-8"))
+            while client_session:
+                print(f"Waiting for message from {addr}")
+                data = conn.recv(1024)
+                if not data:
+                    client_session = False
+                    continue
+
+                decoded = data.decode("utf-8")
+                print(f"Message received from {addr}: {decoded}")
+
+                components = decoded.split("%%")
+                response = "INVALID"
+                match components[0]:
+                    case "ECHO":
+                        if len(components) == 2 and components[1]:
+                            response = components[1]
+
+                conn.sendall(bytes(response, "utf-8"))
+
+            print(f"Client {addr} disconnected.")
